c5e51b46c97b539aa3c6a4f540405d3e5827fa52
Jero <jlopeznavarro@gmail.com>
2019-04-20 17:38:52 +0200

    Updated Ownable class

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -1,24 +1,52 @@
 pragma solidity ^0.5.0;
 
-import 'openzeppelin-solidity/contracts/utils/Address.sol';
-import 'openzeppelin-solidity/contracts/drafts/Counters.sol';
-import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
-import 'openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol';
+import 'node_modules/openzeppelin-solidity/contracts/utils/Address.sol';
+import 'node_modules/openzeppelin-solidity/contracts/drafts/Counters.sol';
+import 'node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol';
+import 'node_modules/openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol';
 import "./Oraclize.sol";
 
 contract Ownable {
     //  TODO's
-    //  1) create a private '_owner' variable of type address with a public getter function
-    //  2) create an internal constructor that sets the _owner var to the creater of the contract 
-    //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
-    //  4) fill out the transferOwnership function
-    //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
+    
+    //  1) create a private '_owner' variable of type address with a public getter function    
+    address private contractOwner;
 
+    function getOwner() public view returns (address) {
+        return contractOwner;
+    }
+
+    //  2) create an internal constructor that sets the _owner var to the creater of the contract 
+    constructor() public 
+    {
+        contractOwner = msg.sender; // set contract owner    
+        emit ownerShipTransfered(contractOwner, address(0));                       
+    }
+
+    //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
+    modifier onlyOwner() {
+        require (msg.sender == contractOwner, "The sender is not the owner of the contract!");
+        _;
+    }
+
+    //  4) fill out the transferOwnership function    
     function transferOwnership(address newOwner) public onlyOwner {
         // TODO add functionality to transfer control of the contract to a newOwner.
         // make sure the new owner is a real address
-
+        
+        // check that the new owner is a real address
+        require(Address.isContract(newOwner) == false, "The new owner can't be a contract address");
+        
+        // set new owner
+        address oldOwner = contractOwner;
+        contractOwner = newOwner;
+        
+        // emit event
+        emit ownerShipTransfered(newOwner, oldOwner);           
     }
+
+    //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
+    event ownerShipTransfered(address newOwner, address oldOwner); 
 }
 
 //  TODO's: Create a Pausable contract that inherits from the Ownable contract
