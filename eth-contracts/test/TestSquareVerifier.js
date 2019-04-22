dfcf63cd2f9b31addc88d2190865ef57395c542a
Jero <jlopeznavarro@gmail.com>
2019-04-22 22:17:17 +0200

    Working with verifier test

--- eth-contracts/test/TestSquareVerifier.js
+++ eth-contracts/test/TestSquareVerifier.js
@@ -3,49 +3,35 @@ const truffleAssert = require('truffle-assertions');
 const proof = require("../../zokrates/code/square/proof.json");
 
 contract('Verifier', accounts => {
-
-    const account_one = accounts[0];    
-
-    describe('Verifier test', function () {
-
-        beforeEach(async function () { 
-            //console.log(Verifier);
-            //this.contract = await Verifier.new();          
-            //this.contract = Verifier;                   
-        })
+         
+    describe('Verifier test', function () {        
 
         it.only('Test verification with correct proof', async function () { 
-         
-            let instance = await Verifier.deployed();
-
-            console.log(proof.proof.a);
-            console.log(proof.proof.b);
-            console.log(proof.proof.c);
-            console.log( proof.inputs);
-
-            let result = await instance.verifyTx(proof.proof.a, proof.proof.b, proof.proof.c, proof.inputs);
-            console.log(result);
-            
-
-                /*
-            Verifier.deployed().then(async function(instance) { 
-                let result = await instance.verifyTx(proof.proof.a, proof.proof.b, proof.proof.c, proof.input);
-                console.log(result);
-                return result;
-            });
-            */
+     
+            let instance = await Verifier.deployed();               
 
             /*
+            this.contract = await Verifier.new({from: accounts[0]});       
             this.contract.events.Verified({
                 fromBlock: 0
             }, function (error, event) {
             
-                assert.equal(err, null, err);
-                console.log(result);           
+                assert.equal(error, null, error);
+                console.log(event);           
             });
-            
-            await this.contract.verifyTx(proof.proof.a, proof.proof.b, proof.proof.c, proof.input);   
-            */        
+            */
+
+            await instance.verifyTx(
+                proof.proof.A, 
+                proof.proof.A_p, 
+                proof.proof.B, 
+                proof.proof.B_p, 
+                proof.proof.C, 
+                proof.proof.C_p, 
+                proof.proof.H, 
+                proof.proof.K, 
+                proof.input
+            );                                 
         })
 
         it('Test verification with incorrect proof', async function () { 
