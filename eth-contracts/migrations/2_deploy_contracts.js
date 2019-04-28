92ed192957b4e3e08b226eac53c7be40a4f6ebb0
Jero <jlopeznavarro@gmail.com>
2019-04-28 10:14:17 +0200

    Changed deploy script

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -3,15 +3,19 @@ var Verifier = artifacts.require("Verifier");
 var SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
 var ERC721MintableComplete = artifacts.require("ERC721MintableComplete");
 
-module.exports = async function(deployer) {
-
-    // ERC721MintableComplete
-    deployer.deploy(ERC721MintableComplete);
+module.exports = function(deployer, network) {
     
-    // Verifier
-    deployer.deploy(Verifier);
-    let contractVerifier = await Verifier.deployed();    
+    // ERC721MintableComplete
+    deployer.deploy(ERC721MintableComplete).then(function() {
+     
+        // Verifier       
+        deployer.deploy(Verifier).then(async function(contractVerifier) {        
+            //let contractVerifier = await Verifier.deployed();    
+        
+            // SolnSquareVerifier
+            deployer.deploy(SolnSquareVerifier, contractVerifier.address);      
+        });
 
-    // SolnSquareVerifier
-    deployer.deploy(SolnSquareVerifier, contractVerifier.address);  
+    });    
+
 };
